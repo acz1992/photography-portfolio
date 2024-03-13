141be841b85cdd0526e848f06e491f5d691ff779
acz19 <acz1992@hotmail.co.uk>
2024-03-13 13:20:06 +0100

    first commit and project set-up

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import HeroSection from "../src/Components/HeroSection";
+import { Container } from "react-bootstrap";
+
+function App() {
+	return (
+		<div>
+			<Container>
+				<HeroSection></HeroSection>
+			</Container>
+		</div>
+	);
+}
+
+export default App;
