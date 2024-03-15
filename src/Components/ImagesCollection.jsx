f80daae7fdfd9a05b3f74067b6fdf9bc8fcc8ec7
acz19 <acz1992@hotmail.co.uk>
2024-03-15 15:07:22 +0100

    Add short tall functionality

--- src/Components/ImagesCollection.jsx
+++ src/Components/ImagesCollection.jsx
@@ -2,21 +2,34 @@ import React from "react";
 
 const ImagesCollection = () => {
 	const imagesList = [...new Array(9)].map(
-		(number, index) => `/assets/images/image-${index + 1}.jpg`
+		(number, index) => `../../src/assets/images/image-${index + 1}.jpg`
 	);
+	console.log(imagesList);
 
 	return (
-		<ul className="images-collection-container">
-			{imagesList.map((imgUrl, index) => {
-				return (
-					<li className="item" key={index}>
-						<figure>
-							<img src={imgUrl} alt="collection" />
-						</figure>
-					</li>
-				);
-			})}
-		</ul>
+		<>
+			<ul className="images-collection-container">
+				{imagesList.map((imgUrl, index) => {
+					return (
+						<li
+							className={
+								index % 2 === 0 ? "item short" : "item tall"
+							}
+							key={index}
+						>
+							<figure>
+								<img src={imgUrl} alt="collection" />
+							</figure>
+						</li>
+					);
+				})}
+			</ul>
+			<div className="d-flex mb-2">
+				<a href="/" className="mx-auto fs-3">
+					View More
+				</a>
+			</div>
+		</>
 	);
 };
 
