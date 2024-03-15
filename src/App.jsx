2df3625a3cdb318f3c11779387210319e6df271f
acz19 <acz1992@hotmail.co.uk>
2024-03-15 13:47:35 +0100

    Add overlay

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Container } from "react-bootstrap";
 import HeroSection from "../src/Components/HeroSection";
 import Header from "../src/Components/Header";
 import React, { useState } from "react";
+import MenuOverlay from "./Components/MenuOverlay";
 
 function App() {
 	const [navbarOpen, setNavbarOpen] = useState(false);
@@ -10,6 +11,10 @@ function App() {
 		<div className="App">
 			<Container>
 				<Header navbarOpen={navbarOpen} setNavbarOpen={setNavbarOpen} />
+				<MenuOverlay
+					navbarOpen={navbarOpen}
+					setNavbarOpen={setNavbarOpen}
+				/>
 				<HeroSection />
 			</Container>
 		</div>
--- src/Components/MenuOverlay.jsx
+++ src/Components/MenuOverlay.jsx
@@ -1,7 +1,30 @@
 import React from "react";
 
 const MenuOverlay = ({ navbarOpen, setNavbarOpen }) => {
-	return <div></div>;
+	return (
+		<nav className={`menu-container ${navbarOpen ? "expanded" : ""} `}>
+			<ul className="menu-list">
+				<li className="nav-li">
+					<a
+						href="/"
+						className="nav-link"
+						onClick={() => setNavbarOpen(false)}
+					>
+						Home
+					</a>
+				</li>
+				<li className="nav-li">
+					<a
+						href="/"
+						className="nav-link"
+						onClick={() => setNavbarOpen(false)}
+					>
+						About
+					</a>
+				</li>
+			</ul>
+		</nav>
+	);
 };
 
 export default MenuOverlay;
